b6cdf993f1be89def67d1ff073c8f6aa3986cc8c
Alexme116 <alexmoncada16@hotmail.com>
2025-05-12 12:09:34 -0600

    Base

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.MyTodoList.repository;
-
-import com.springboot.MyTodoList.model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TeamWorkingModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.MyTodoList.repository;
-
-import com.springboot.MyTodoList.model.TeamWorkingModule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamWorkingModuleRepository extends JpaRepository<TeamWorkingModule, Integer> {
-}
