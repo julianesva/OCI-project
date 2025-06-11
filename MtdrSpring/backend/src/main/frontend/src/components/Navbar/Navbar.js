41fc5cba744245492685c529b07ced1ce2492cab
Santiago Gutierrez Gonzalez <117130198+santigugon@users.noreply.github.com>
2025-06-11 12:47:38 -0600

    Update Navbar.js test

--- MtdrSpring/backend/src/main/frontend/src/components/Navbar/Navbar.js
+++ MtdrSpring/backend/src/main/frontend/src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
             {/* Logo Container */}
             <div className="navbar-logo-container">
                 <img src={O_Oracle_Icon} alt="Logo" className="navbar-logo-icon" />
-                <span className="navbar-logo-text">| Oracle Manager</span>
+                <span className="navbar-logo-text">| Oracules Manager</span>
             </div>
 
             {/* User Container */}
